7d92af1b99420b5e28168a7c27b8b42631cb7eb6
henshuuuuuuuuuuuu <henshusempai@gmail.com>
2025-11-15 05:31:07 +0800

    Add loading screen functionality on window load

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loading-screen');
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 1500);
+});
